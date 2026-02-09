8e896137a954fb474182d5ec000fd6a02ce2eaa6
Punnapa Pramotekul <punnapa.pr@ku.th>
2026-02-09 21:05:17 +0700

    (test): add more video

--- example-4/fixtures/two-videos/videos.js
+++ example-4/fixtures/two-videos/videos.js
@@ -8,5 +8,9 @@ module.exports = [
     {
         _id: new ObjectId("5ea234a5c34230004592eb33"),
         name: "Another video.mp4"
+    },
+    {
+        _id: new ObjectId("5ea234a5c34230004592eb33"),
+        name: "Failed video.mp4"
     }
 ];
